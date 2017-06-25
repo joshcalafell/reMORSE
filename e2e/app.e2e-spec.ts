58ea275134c5daee36c56202fd14de07c84fa197
rabbitfighter81 <rabbitfighter@cryptolab.net>
2017-06-25 10:13:32 -0600

    e2e tests working now

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('js-phonetic-alphabet App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Morse Code Translator');
   });
 });
