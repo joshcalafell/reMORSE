5140013e1adb2983500b34323f324ef50f79f327
rabbitfighter81 <rabbitfighter@cryptolab.net>
2017-06-29 10:54:41 -0600

    Updated SASS and some HTML

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'M●rse C●de Translat●r';
   words:string[] = [];
 
-  public updateText(event): void {
+  public updateText(event: any): void {
     this.words = this.getTextArray(event.target.value);
   }
 
-  public getTextArray(text:string): string[] {
+  public getTextArray(text: string): string[] {
     return text.match(/\b(\w+)\b/g) || [];
   }
 
